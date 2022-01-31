1c046a3001986c2aabd2ca2a5ac90f7c4b4e71eb
BiancaSozzi <b.sozzi@eyeo.com>
2022-01-31 11:29:17 +0100

    question activity wip

--- app/src/main/java/com/example/quizapp/QuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuestionActivity.kt
@@ -2,13 +2,30 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuestionActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var btnSubmit: Button? = null
+    private var tvQuestion: TextView? = null
+    private var tvImage: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
 
         val questionsList = Constants.getQuestions()
+
+        progressBar = findViewById(R.id.progress_bar)
+        tvProgress = findViewById(R.id.tv_progress)
+        btnSubmit = findViewById(R.id.btn_submit)
+        tvQuestion = findViewById(R.id.tv_question)
+        tvImage = findViewById(R.id.iv_flag)
 
     }
 }
